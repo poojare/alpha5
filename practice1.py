3123b1858106381e2f288dc784279bc1f3aa4009
poojare <poojareddy.patil6@gmail.com>
2022-04-28 11:12:09 +0530

    adding practice file to git

---
+++ practice1.py
@@ -0,0 +1,13 @@
+mes="hi my name is %s and I am %d years old"%("john",25)
+print(mes)
+mes1="The value of pi is %0.4f"%(3.141592)
+print(mes1)
+string="Hi Welcome to python"
+print(string[::2])
+
+print(string[::-2])
+print(string[::-1])
+soo='youtube.txt'
+print(soo[8:])
+print(soo[7:])
+print(soo[:7])
